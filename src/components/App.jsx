47d0cd9f09b280b1a12651037c52b723b87d5eaa
wellingtonflores <wellingtonluiscardoso@hotmail.com>
2024-07-31 18:35:56 -0300

    adicioandno o router no app e tirando do main

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Registro from './Registro';
 import PaginaInicial from "./PaginaInicial";
 import Footer from "./Footer";
@@ -12,16 +12,20 @@ import MeusAgendamentos from "./MeusAgendamentos"
 function App() {
   return (
     <div>
-      <Header/>
-      <Routes>
-        <Route path="/" element={<PaginaInicial/>}/>
-        <Route path="/registro" element={<Registro />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/perfil" element={<Perfil/>}/>
-        <Route path="/agendar" element={<Agendamento/>}/>
-        <Route path="/agendamentos" element={<MeusAgendamentos/>}/>
-      </Routes>
-    <Footer/>
+
+      <Router>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<PaginaInicial/>}/>
+          <Route path="/registro" element={<Registro />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/perfil" element={<Perfil/>}/>
+          <Route path="/agendar" element={<Agendamento/>}/>
+          <Route path="/agendamentos" element={<MeusAgendamentos/>}/>
+        </Routes>
+         <Footer/>
+      </Router>
+     
     </div>
     
   );
